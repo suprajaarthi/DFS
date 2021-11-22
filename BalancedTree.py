e84e23fa20d3d2b7c70e811060e8bf2f2b473323
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2021-11-22 19:27:38 +0530

    Create BalancedTree.py

---
+++ BalancedTree.py
@@ -0,0 +1,54 @@
+https://leetcode.com/problems/balanced-binary-tree/
+  
+class Solution(object):
+    def isBalanced(self, root):
+        def calculate(node):
+            print(node)
+            if not node: return 0
+            myself = 1
+            left = calculate(node.left)
+            print(left)
+            right = calculate(node.right)
+            print(right)
+            if left < 0 or right < 0: return -1
+            if abs(left - right) > 1: return -1
+            print(myself+max(left,right))
+            return 1 + max(left, right)
+                                
+        return False if calculate(root) < 0 else True
+      
+      
+    Given a binary tree, determine if it is height-balanced.
+
+For this problem, a height-balanced binary tree is defined as:
+
+a binary tree in which the left and right subtrees of every node differ in height by no more than 1.
+
+ 
+
+Example 1:
+
+      3
+      |
+    9   20
+        | 
+      15   7
+Input: root = [3,9,20,null,null,15,7]
+Output: true
+Example 2:
+
+
+Input: root = [1,2,2,3,3,null,null,4,4]
+Output: false
+Example 3:
+      1
+      |
+    2   2
+    |
+  3  3
+  |
+4  4 
+
+
+Input: root = []
+Output: true
