a71aba1b2a67597dabca2a86dc697c7cc19d679c
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2021-11-22 19:28:12 +0530

    Update BalancedTree.py

--- BalancedTree.py
+++ BalancedTree.py
@@ -18,37 +18,32 @@ class Solution(object):
         return False if calculate(root) < 0 else True
       
       
-    Given a binary tree, determine if it is height-balanced.
+# Given a binary tree, determine if it is height-balanced. For this problem, a height-balanced binary tree is defined as
+# a binary tree in which the left and right subtrees of every node differ in height by no more than 1.
 
-For this problem, a height-balanced binary tree is defined as:
+# Example 1:
 
-a binary tree in which the left and right subtrees of every node differ in height by no more than 1.
-
- 
-
-Example 1:
-
-      3
-      |
-    9   20
-        | 
-      15   7
-Input: root = [3,9,20,null,null,15,7]
-Output: true
-Example 2:
+#       3
+#       |
+#     9   20
+#         | 
+#       15   7
+# Input: root = [3,9,20,null,null,15,7]
+# Output: true
+# Example 2:
 
 
-Input: root = [1,2,2,3,3,null,null,4,4]
-Output: false
-Example 3:
-      1
-      |
-    2   2
-    |
-  3  3
-  |
-4  4 
+# Input: root = [1,2,2,3,3,null,null,4,4]
+# Output: false
+# Example 3:
+#       1
+#       |
+#     2   2
+#     |
+#   3  3
+#   |
+# 4  4 
 
 
-Input: root = []
-Output: true
+# Input: root = []
+# Output: true
